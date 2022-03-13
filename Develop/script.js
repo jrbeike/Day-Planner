53bcc00d2f4ca73aa51f05e68830697f62394390
jrbeike <jrbeike@gmail.com>
2022-03-13 17:28:54 -0400

    Update script.js

---
+++ Develop/script.js
@@ -0,0 +1,12 @@
+//set up the list for local storage 
+//display current d/m/y at top of the page
+//set time and color for tasks based on time stamp
+//remove all classes
+//load tasks on load
+//read tasks from storage
+//update time second every 1000ms
+//on task click highligh task
+//highlight unsaved tasks
+//indicate changes are unsaved
+//on saave update tasks in local storage
+//promts to save unsaved chages before page reload
